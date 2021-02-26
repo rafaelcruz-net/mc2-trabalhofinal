b10878f59d91ae374db3346c17c2f64a612b8610
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-02-26 17:52:07 -0300

    Exporta o grafico para imagem

--- script/question7.r
+++ script/question7.r
@@ -1,32 +1,33 @@
 rm(list = ls());
-#setwd("/var/RRR"); #setar o diretorio
+library("ggplot2");
+setwd("~/Mestrado/TrabalhoMC2");
 
-mar_file <- read.table("data_t7_Margarine_error1_frontier_obj.txt",header=TRUE);
+mar_file <- read.table("data/data_t7_Margarine_error1_frontier_obj.txt",header=TRUE);
 mar_ACAD <- subset(mar_file, inst == 'ACAD');
 mar_ACAD$Arquivo<-'MAR_ACAD'
 mar_PARM <- subset(mar_file, inst == 'PARM');
 mar_PARM$Arquivo<-'MAR_PARM'
 
-cpm_file <- read.table("data_t7_CPM_error1_frontier_obj.txt",header=TRUE);
+cpm_file <- read.table("data/data_t7_CPM_error1_frontier_obj.txt",header=TRUE);
 cpm_ACAD <- subset(cpm_file, inst == 'ACAD');
 cpm_ACAD$Arquivo<-'CPM_ACAD'
 cpm_PARM <- subset(cpm_file, inst == 'PARM');
 cpm_PARM$Arquivo<-'CPM_PARM'
 
-nensga_file <- read.table("data_t7_nsga_150k_c50_2x_error1_frontier_obj.txt",header=TRUE);
+nensga_file <- read.table("data/data_t7_nsga_150k_c50_2x_error1_frontier_obj.txt",header=TRUE);
 nensga_ACAD <- subset(nensga_file, inst == 'ACAD');
 nensga_ACAD$Arquivo<-'nensga_ACAD'
 nensga_PARM <- subset(nensga_file, inst == 'PARM');
 nensga_PARM$Arquivo<-'nensga_PARM'
 
 
-nsga_file <- read.table("data_t7_nsga_150k_c50_2x_noerror_frontier_obj.txt",header=TRUE);
+nsga_file <- read.table("data/data_t7_nsga_150k_c50_2x_noerror_frontier_obj.txt",header=TRUE);
 nsga_ACAD <- subset(nsga_file, inst == 'ACAD');
 nsga_ACAD$Arquivo<-'nsga_ACAD'
 nsga_PARM <- subset(nsga_file, inst == 'PARM');
 nsga_PARM$Arquivo<-'nsga_PARM'
 
-sh_file <- read.table("data_t7_SecondHalf_error1_frontier_obj.txt",header=TRUE);
+sh_file <- read.table("data/data_t7_SecondHalf_error1_frontier_obj.txt",header=TRUE);
 sh_ACAD <- subset(sh_file, inst == 'ACAD');
 sh_ACAD$Arquivo<-'SH_ACAD'
 sh_PARM <- subset(sh_file, inst == 'PARM');
@@ -44,6 +45,8 @@ PARM <- rbind(mar_PARM,cpm_PARM,nensga_PARM,nsga_PARM,sh_PARM)
 
 attach(ACAD)
 
+# SALVA O GRAFICO 1
+png("data/result/graf1_acad.png", width = 1024, height = 768, res = 92);
 graf1_acad <- ggplot(ACAD, aes(mks, cst))+
   geom_point(aes(color = Arquivo, shape = Arquivo))+
 #  stat_smooth(aes(color = Arquivo, fill = Arquivo), 
@@ -52,8 +55,11 @@ graf1_acad <- ggplot(ACAD, aes(mks, cst))+
   scale_fill_manual(values = c("#E4F00A", "white", "#22FF00","pink","orange")) 
 
 graf1_acad + theme_dark()
+print(graf1_acad);  
+dev.off();
 
-
+# SALVA O GRAFICO 2
+png("data/result/graf2_acad.png", width = 1024, height = 768, res = 92);
 
 graf2_acad <- ggplot(ACAD, aes(noh, mks))+
   geom_point(aes(color = Arquivo, shape = Arquivo))+
@@ -63,9 +69,12 @@ graf2_acad <- ggplot(ACAD, aes(noh, mks))+
   scale_fill_manual(values = c("#E4F00A", "white", "#22FF00","pink","orange")) 
 
 graf2_acad + theme_dark()
+print(graf2_acad);
+dev.off();
 
 
 
+png("data/result/graf3_acad.png", width = 1024, height = 768, res = 92);
 graf3_acad <- ggplot(ACAD, aes(noh, cst))+
   geom_point(aes(color = Arquivo, shape = Arquivo))+
   #  stat_smooth(aes(color = Arquivo, fill = Arquivo), 
@@ -75,11 +84,12 @@ graf3_acad <- ggplot(ACAD, aes(noh, cst))+
 
 graf3_acad + theme_dark()
 
-  
-  
+print(graf3_acad);  
+dev.off();
 
 attach(PARM)
-  
+
+png("data/result/graf1_param.png", width = 1024, height = 768, res = 92);  
 graf1_parm <- ggplot(PARM, aes(mks, cst))+
     geom_point(aes(color = Arquivo, shape = Arquivo))+
     #  stat_smooth(aes(color = Arquivo, fill = Arquivo), 
@@ -88,9 +98,11 @@ graf1_parm <- ggplot(PARM, aes(mks, cst))+
     scale_fill_manual(values = c("#E4F00A", "white", "#22FF00","pink","orange")) 
   
 graf1_parm + theme_dark()  
-
+print(graf1_parm);
+dev.off();
   
 
+png("data/result/graf2_param.png", width = 1024, height = 768, res = 92);  
 graf2_parm <- ggplot(PARM, aes(noh, mks))+
   geom_point(aes(color = Arquivo, shape = Arquivo))+
   #  stat_smooth(aes(color = Arquivo, fill = Arquivo), 
@@ -99,9 +111,10 @@ graf2_parm <- ggplot(PARM, aes(noh, mks))+
   scale_fill_manual(values = c("#E4F00A", "white", "#22FF00","pink","orange")) 
 
 graf2_parm + theme_dark() 
+print(graf2_parm);
+dev.off();
 
-
-
+png("data/result/graf3_param.png", width = 1024, height = 768, res = 92);  
 graf3_parm <- ggplot(PARM, aes(noh, cst))+
   geom_point(aes(color = Arquivo, shape = Arquivo))+
   #  stat_smooth(aes(color = Arquivo, fill = Arquivo), 
@@ -110,5 +123,9 @@ graf3_parm <- ggplot(PARM, aes(noh, cst))+
   scale_fill_manual(values = c("#E4F00A", "white", "#22FF00","pink","orange")) 
 
 graf3_parm + theme_dark()  
-  
+
+print(graf3_parm);
+dev.off();
+
+
  
